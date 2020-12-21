d39c5cb5aaadf2f93f6cf359d9d111e112485a31
Ethan <48326421+EthanKoland@users.noreply.github.com>
2020-12-21 07:20:52 -0600

    Update M-94_code.py

--- M-94_code.py
+++ M-94_code.py
@@ -105,6 +105,67 @@ def main():
         encryptEntry.delete(0, tk.END)
         encryptEntry.insert(0,out)
                 
+
+    def decrypt(s):
+        window1 = tk.Tk()
+    
+
+        window1.rowconfigure([0,1], minsize = 100, weight=1)
+        window1.columnconfigure([0,1,2], minsize=25, weight=1)
+
+        exitText = tk.Label(master=window1, text="hello", background="blue")
+        exitText.grid(row=0, column=0, columnspan=3, sticky="nsew")
+
+        infoText = tk.Label(master=window1, text = "Enter a encrypted Text")
+        infoText.grid(column=0, row=1, sticky="nsew")
+
+        encryptEntry = tk.Entry(master=window1)
+        encryptEntry.grid(row=1, column=1,  sticky="nsew")
+
+        encryptButton = tk.Button(master=window1, text="encrypt Message", command = lambda: decrypt(encryptEntry.get(), exitText) )
+        encryptButton.grid(row=1, column=2, sticky="NSEW")
+        
+        encryptedText = s
+
+        if(len(encryptedText) > 25):
+            return
+        
+        wheels = []
+        with open("m-94wheels.txt", "r") as txt:
+            for line in txt:
+                wheels.append(line[0:len(line) -1])
+
+    
+
+        wheels = adjustStarting(wheels, encryptedText)
+    
+
+        possible = []
+        for i in range(len(wheels[0])):
+            temp = ""
+            for  t in range(len(wheels)):
+                temp += wheels[t][i]
+            possible.append(temp)
+
+   
+
+        dict = []
+        with open("dictionary.txt", "r") as txt:
+            dict = txt.read().split()
+
+        ratio = {}
+        for p in possible:
+        
+            ratio[p] = isEnglish(p, dict)
+
+        exitString = []
+
+        for r in ratio:
+            exitString.append(str(r) + " : " + str(ratio[r]) + "\n")
+
+    
+
+        exitText["text"] = exitString
             
             
 
@@ -142,7 +203,7 @@ def main():
     encryptButton = tk.Button(master=window, text="encrypt Message", command=lambda: encrypt(plainEntry.get()))
     encryptButton.grid(row=3, column=26, sticky="NSEW")
 
-    decryptButton = tk.Button(master=window, text="Decrypt Message", command=lambda: encrypt(encryptEntry.get()))
+    decryptButton = tk.Button(master=window, text="Decrypt Message", command=lambda: decrypt(encryptEntry.get()))
     decryptButton.grid(row=0, column=26, sticky="NSEW")
 
 
@@ -496,6 +557,24 @@ def main():
     
     window.mainloop()
     
+def isEnglish(s, dict):
+    c = 0
+    for i in range(len(s)):
+        for t in range(i, len(s)):
+            if s[i : t+1].upper() in dict:
+                c += 1
+                
+
+    return(c)
+
+
+def adjustStarting(t, m):
+    for i in range(len(m)):
+        char = m[i]
+        
+        ind = t[i].find(char)
+        t[i] = t[i][ind : len(t[i])] + t[i][0 : ind]
+    return t
 
 if (__name__ == "__main__"):
     main()
